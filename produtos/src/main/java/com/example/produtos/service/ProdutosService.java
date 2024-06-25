a60d0589f85cca250287f6d07068347d0bfb876e
Ester Oliveira <oliver.ester.2004@gmail.com>
2024-06-24 22:43:08 -0300

    crud produtos

--- produtos/src/main/java/com/example/produtos/service/ProdutosService.java
+++ produtos/src/main/java/com/example/produtos/service/ProdutosService.java
@@ -1,6 +1,9 @@
 package com.example.produtos.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +18,47 @@ public class ProdutosService {
     @Autowired
     private ProdutosRepository repository;
 
+    public Optional<ProdutosDTO> encontrarProduto(UUID id){
+        Optional<Produtos> prod_01 = repository.findById(id);
+        if(prod_01.isEmpty()){
+            throw new RuntimeException("Produto não existe.");
+        }
+        ProdutosDTO prod_02 = new ProdutosDTO();
+        BeanUtils.copyProperties(prod_01, prod_02);
+        return Optional.of(prod_02);
+    }
+
+    public List<ProdutosDTO> listarProdutos(){
+        List<Produtos> prods_01 = repository.findAll();
+        List<ProdutosDTO> prods_02 = new ArrayList<>();
+        BeanUtils.copyProperties(prods_01, prods_02);
+        return prods_02;
+    }
+
     public ProdutosDTO adicionarProdutos(ProdutosDTO prod){
-        Produtos prod_novo = new Produtos();
-        BeanUtils.copyProperties(prod, prod_novo);
-        repository.save(prod_novo);
+        Produtos prod_01 = new Produtos();
+        BeanUtils.copyProperties(prod, prod_01);
+        repository.save(prod_01);
         return prod;
     }
 
-    public ProdutosDTO atualizaProdutos(ProdutosDTO prod){
-        Optional<Produtos> prod_atualizado = repository.findById(prod.getId());
-        if(prod_atualizado.isEmpty()){
+    public Optional<ProdutosDTO> atualizaProdutos(ProdutosDTO prod){
+        Optional<Produtos> prod_01= repository.findById(prod.getId());
+        if(prod_01.isEmpty()){
             throw new RuntimeException("Produto não existe.");
         }
-        Produtos prod_atualizado_novo = prod_atualizado.get();
-        repository.save(prod_atualizado_novo);
-        return prod;
+        Produtos prod_02 = prod_01.get();
+        repository.save(prod_02);
+        return Optional.of(prod);
+    }
+
+    public Optional<ProdutosDTO> deletarProduto(ProdutosDTO prod){
+        Optional<Produtos> prod_01 = repository.findById(prod.getId());
+        if(prod_01.isEmpty()){
+            throw new RuntimeException("Produto não existe.");
+        }
+        Produtos prod_02 = prod_01.get();
+        repository.delete(prod_02);
+        return Optional.of(prod);
     }
 }
--- produtos/src/main/java/com/example/produtos/view/ProdutosController.java
+++ produtos/src/main/java/com/example/produtos/view/ProdutosController.java
@@ -1,8 +1,83 @@
 package com.example.produtos.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.produtos.service.ProdutosService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.produtos.model.ProdutosDTO;
+import com.example.produtos.model.ProdutosRequest;
+import com.example.produtos.model.ProdutosResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
 @RestController
+@RequestMapping("/produtos")
 public class ProdutosController {
+    @Autowired
+    private ProdutosService service;
+
+    @GetMapping("")
+    public ResponseEntity<List<ProdutosResponse>> listarProdutos() {
+        List<ProdutosDTO> produtos = service.listarProdutos();
+        List<ProdutosResponse> produtos_response = new ArrayList<>();
+        BeanUtils.copyProperties(produtos, produtos_response);
+        return new ResponseEntity<>(produtos_response, HttpStatus.OK);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<ProdutosResponse>> encontrarProduto(@PathVariable UUID id) {
+        Optional<ProdutosDTO> prod = service.encontrarProduto(id);
+        ProdutosResponse prod_response = new ProdutosResponse();
+        BeanUtils.copyProperties(prod, prod_response);
+        return new ResponseEntity<>(Optional.of(prod_response), HttpStatus.OK);
+    }
+
+    @PostMapping("/adicionar")
+    public ResponseEntity<ProdutosResponse> adicionarProdutos(@RequestBody ProdutosRequest prod) {
+        ProdutosDTO produto = new ProdutosDTO();
+        BeanUtils.copyProperties(prod, produto);
+        service.adicionarProdutos(produto);
+        ProdutosResponse produto_response = new ProdutosResponse();
+        BeanUtils.copyProperties(prod, produto_response);
+        return new ResponseEntity<>(produto_response, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/atualizar")
+    public ResponseEntity<Optional<ProdutosResponse>> atualizarProdutos(@RequestBody ProdutosRequest prod) {
+        ProdutosDTO produto = new ProdutosDTO();
+        BeanUtils.copyProperties(prod, produto);
+        service.atualizaProdutos(produto);
+        ProdutosResponse produto_response = new ProdutosResponse();
+        BeanUtils.copyProperties(prod, produto_response);
+        return new ResponseEntity<>(Optional.of(produto_response), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deletar")
+    public ResponseEntity<Optional<ProdutosResponse>> deletarProdutos(@RequestBody ProdutosRequest prod){
+        ProdutosDTO produto = new ProdutosDTO();
+        BeanUtils.copyProperties(prod, produto);
+        service.deletarProduto(produto);
+        ProdutosResponse produto_response = new ProdutosResponse();
+        BeanUtils.copyProperties(prod, produto_response);
+        return new ResponseEntity<>(Optional.of(produto_response), HttpStatus.ACCEPTED);
+    }
+    
     
 }
