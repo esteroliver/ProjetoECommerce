e05395432433f648bcb9e2fefa2a2ea0b2e3bf59
Ester Oliveira <oliver.ester.2004@gmail.com>
2024-06-26 11:19:33 -0300

    correções

--- pedidos/src/main/java/com/example/pedidos/PedidosApplication.java
+++ pedidos/src/main/java/com/example/pedidos/PedidosApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClient
+@EnableFeignClients
 public class PedidosApplication {
 
 	public static void main(String[] args) {
---
+++ pedidos/src/main/java/com/example/pedidos/models/ProdutosResponse.java
@@ -0,0 +1,41 @@
+package com.example.pedidos.models;
+
+public class ProdutosResponse {
+    private Double valor;
+    private String nome;
+    private Integer quantidade;
+    private String descricao;
+    public ProdutosResponse() {
+    }
+    public ProdutosResponse(Double valor, String nome, Integer quantidade, String descricao) {
+        this.valor = valor;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.descricao = descricao;
+    }
+    public Double getValor() {
+        return valor;
+    }
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
